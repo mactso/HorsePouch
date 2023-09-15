6931bc2294a2da0cab04af71252ef2fa08fd23a4
mactso <stephenmcleod2004@yahoo.com>
2023-09-15 07:39:12 -0500

    Horsepouch 36.1.1 for MC 1.16.3-1.16.5

--- src/main/java/com/mactso/horsepouch/items/HorsePouchItem.java
+++ src/main/java/com/mactso/horsepouch/items/HorsePouchItem.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.mactso.horsepouch.config.MyConfig;
 import com.mactso.horsepouch.utility.Utility;
 
 import net.minecraft.block.BlockState;
@@ -13,18 +14,12 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.merchant.villager.VillagerEntity;
-import net.minecraft.entity.passive.IronGolemEntity;
-import net.minecraft.entity.passive.horse.AbstractHorseEntity;
 import net.minecraft.entity.passive.horse.HorseEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
-import net.minecraft.item.SpawnEggItem;
 import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.nbt.INBT;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Direction;
@@ -32,11 +27,11 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.EntityRayTraceResult;
-import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.util.text.Color;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
@@ -57,59 +52,67 @@ public class HorsePouchItem extends Item {
 			return ActionResultType.PASS;
 		}
 
-		LOGGER.info("interactLivingEntity on server.");
-
-		if (entity instanceof AbstractHorseEntity) {
-			AbstractHorseEntity ahe = (AbstractHorseEntity) entity;
-			if (player.getUUID().equals(ahe.getOwnerUUID())) {
-				ahe.ejectPassengers();
-				CompoundNBT entityData = entity.serializeNBT();
-				entityData.remove("Pos");
-				entityData.remove("UUID");
-				itemStack.getOrCreateTag().put("StoredEntityData", entityData);
-				entity.remove();
-				Utility.debugMsg(1, "Remove Horse");
-
-				world.playSound(null, player.blockPosition(), SoundEvents.HORSE_ARMOR, SoundCategory.PLAYERS, 1.0F,
-						1.0F);
-				return ActionResultType.CONSUME;
-			}
-
+		Utility.debugMsg(1, "Horse Pouch Storing Horse");
+		if (!(entity instanceof HorseEntity)) {
+			return ActionResultType.PASS;
 		}
 
-		return ActionResultType.PASS;
+		HorseEntity targetHorse = (HorseEntity) entity;
+
+		if ((MyConfig.isMustBeOwner()) && (!player.getUUID().equals(targetHorse.getOwnerUUID()))) {
+			return ActionResultType.PASS;
+		}
+
+		if ((MyConfig.isMustBeSaddled()) && !(targetHorse.isSaddled())) {
+			return ActionResultType.PASS;
+		}
+
+		targetHorse.ejectPassengers();
+		CompoundNBT entityData = entity.serializeNBT();
+		entityData.remove("Pos");
+		entityData.remove("UUID");
+		// entityData.removeTag("Dimension"); was in 12.
+		itemStack.getOrCreateTag().put("StoredEntityData", entityData);
+		entity.remove();
+
+		world.playSound(null, player.blockPosition(), SoundEvents.HORSE_ARMOR, SoundCategory.PLAYERS, 1.0F, 1.0F);
+		return ActionResultType.CONSUME;
+
 	}
 
 	@Override
 	public void appendHoverText(ItemStack itemStack, World world, List<ITextComponent> tiplist,
 			ITooltipFlag p_77624_4_) {
-		CompoundNBT entityData = itemStack.getOrCreateTag().getCompound("StoredEntityData");
-		if (entityData.size() > 0) {
-			EntityType<?> entityType = EntityType.byString(entityData.getString("id")).orElse(null);
-			if (entityData.contains("CustomName", 8)) {
-				String nameValue = entityData.getString("CustomName");
-				try {
-					IFormattableTextComponent name = ITextComponent.Serializer.fromJson(nameValue);
-					StringTextComponent named = new StringTextComponent(
-							"Holding a " + entityType.getRegistryName().getPath() + " named ");
-					named.append(name);
-					tiplist.add(named);
-					return;
-				} catch (Exception exception) {
-					LOGGER.warn("Failed to parse entity custom name {}", nameValue, exception);
-				}
-			}
 
-			tiplist.add(new StringTextComponent("Holding a " + entityType.getRegistryName().getPath()));
-		} else {
-			tiplist.add(new StringTextComponent("Empty"));
+		CompoundNBT nbtTag = itemStack.getTag(); // Correct method
+
+		if ((nbtTag == null) || (!(nbtTag.contains("StoredEntityData")))) {
+			tiplist.add(new StringTextComponent("Not holding a steed."));
+			return;
 		}
 
+		CompoundNBT entityData = nbtTag.getCompound("StoredEntityData");
+		EntityType<?> entityType = EntityType.byString(entityData.getString("id")).orElse(null);
+		StringTextComponent bagTip = new StringTextComponent(
+				"Holding a " + entityType.getRegistryName().getPath() + ".");
+		if (entityData.contains("CustomName", 8)) {
+			IFormattableTextComponent name = new StringTextComponent(" ?Name? ");
+			try {
+				name = ITextComponent.Serializer.fromJson(entityData.getString("CustomName"));
+			} catch (Exception e) {
+				// failure to parse steed name isn't fatal.
+			}
+			bagTip = new StringTextComponent("Holding a " + entityType.getRegistryName().getPath() + " named ");
+			bagTip.append(name);
+		}
+		tiplist.add(bagTip);
 	}
 
 	@Override
 	public ActionResultType useOn(ItemUseContext iuc) {
-		if (!iuc.getLevel().isClientSide) {
+
+		World level = iuc.getLevel();
+		if (!level.isClientSide) {
 			World world = iuc.getLevel();
 			PlayerEntity player = iuc.getPlayer();
 			Hand hand = iuc.getHand();
@@ -128,7 +131,6 @@ public class HorsePouchItem extends Item {
 			CompoundNBT entityData = itemStack.getOrCreateTag().getCompound("StoredEntityData");
 			if (entityData.contains("id", 8)) {
 				Utility.debugMsg(1, "Restore Horse");
-				LOGGER.info("ReStore Horse");
 				EntityType<?> entityType = EntityType.byString(entityData.getString("id")).orElse(null);
 				if (entityType != null) {
 					CompoundNBT newEntityData = new CompoundNBT();
@@ -138,8 +140,12 @@ public class HorsePouchItem extends Item {
 
 					// note: boolean returned by this is unreliable;
 					boolean bool = ((ServerWorld) world).addFreshEntity(newEntity);
-					int i = 4;
 					itemStack.getOrCreateTag().remove("StoredEntityData");
+
+					StringTextComponent component = new StringTextComponent("Your steed restored nearby.");
+					component.getStyle().withBold(true);
+					component.getStyle().withColor(Color.fromLegacyFormat(TextFormatting.DARK_GREEN));
+					player.sendMessage(component, player.getUUID());
 
 					return ActionResultType.CONSUME;
 				}
@@ -170,6 +176,11 @@ public class HorsePouchItem extends Item {
 					boolean bool = ((ServerWorld) world).addFreshEntity(newEntity);
 					int i = 4;
 					itemStack.getOrCreateTag().remove("StoredEntityData");
+
+					StringTextComponent component = new StringTextComponent("Your steed restored where you are.");
+					component.getStyle().withBold(true);
+					component.getStyle().withColor(Color.fromLegacyFormat(TextFormatting.DARK_GREEN));
+					player.sendMessage(component, player.getUUID());
 
 					return ActionResult.consume(itemStack);
 				}
