bbbcf97b4581ef7e39163e48994b31fb2b469ce6
Mac <Mac@192.168.12.190>
2025-09-03 23:10:37 -0500

    Version 1.1 for Minecraft 1.21.5-1.21.8 for Fabric
    Had to make a change to the mapping resolution code so it would work in
    1.21.5 and also work in 1.21.6 thru 1.21.8.

--- src/main/java/com/mactso/horsepouch/MainClient.java
+++ src/main/java/com/mactso/horsepouch/MainClient.java
@@ -5,7 +5,8 @@ import java.lang.reflect.Field;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-
+import net.fabricmc.loader.api.FabricLoader;
+import net.fabricmc.loader.api.MappingResolver;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.ExtraCodecs.LateBoundIdMapper;
 import net.minecraft.client.renderer.item.properties.conditional.ConditionalItemModelProperties;
@@ -21,18 +22,33 @@ public class MainClient implements ClientModInitializer {
 	@SuppressWarnings("unchecked")
 	@Override
 	public void onInitializeClient() {
+		
 		Field field_ID_MAPPER = null;
 
+        MappingResolver mapper = FabricLoader.getInstance().getMappingResolver();
+        String name = mapper.mapFieldName("intermediary", "net.minecraft.class_10459", "field_55372", "Lnet/minecraft/class_5699$class_10388;"); // ID_MAPPER
+        
 		try {
-			field_ID_MAPPER = ConditionalItemModelProperties.class.getDeclaredField("ID_MAPPER");
+			field_ID_MAPPER = ConditionalItemModelProperties.class.getDeclaredField(name);
 			field_ID_MAPPER.setAccessible(true);
 		} catch (Exception e) {
-			e.printStackTrace();
-			return;
+			// e.printStackTrace();
+			// return;
 		}
+		
+//		if (field_ID_MAPPER == null) {
+//			try {
+//				field_ID_MAPPER = ConditionalItemModelProperties.class.getDeclaredField("ID_MAPPER");
+//				field_ID_MAPPER.setAccessible(true);
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//				// return;
+//			}
+//		}
+		
 
 		LateBoundIdMapper<ResourceLocation, MapCodec<? extends ConditionalItemModelProperty>> theirIdMapper;
-
+		int debug = 3;
 			try {
 				// this causes an unchecked cast warning
 				theirIdMapper = (LateBoundIdMapper<ResourceLocation, MapCodec<? extends ConditionalItemModelProperty>>) field_ID_MAPPER
